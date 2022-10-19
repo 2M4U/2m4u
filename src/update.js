53b14d839eef6c884e86164eab76ec298149ddc1
2M4U <cybercdn.host@gmail.com>
2022-10-19 19:01:56 +0100

    Update update.js

--- src/update.js
+++ src/update.js
@@ -1,7 +1,17 @@
 const { join } = require("path");
 const fetch = require("node-fetch");
 const { writeFileSync } = require("fs");
+let stars = 0,
+  page = 1;
 
+const CountStars = async () => {
+  let StarsData = await fetch(
+    `https://api.github.com/users/2M4U/starred?per_page=100&page=${page}`
+  ).then((res) => res.json());
+  stars += StarsData.length;
+  page++;
+  if (StarsData.length === 100) CountStars();
+};
 const WriteReadMe = async () => {
   const ReadMe = join(__dirname, "..", "README.md");
   const date = new Date();
@@ -10,11 +20,14 @@ const WriteReadMe = async () => {
     `https://fortnite-api.com/v2/stats/br/v2?name=${process.env.FORTNITE_USERNAME}`,
     {
       headers: {
-        Authorization: process.env.API_SECRET, 
+        Authorization: process.env.API_SECRET,
       },
     }
   ).then((res) => res.json());
- 
+  let UserData = await fetch("https://api.github.com/users/SudhanPlayz").then(
+    (res) => res.json()
+  );
+  console.log(UserData);
   const text = `
   Welcome **Github User** to the Code Land of 2M4U (me),<br>
   What you see below is a future project for updating my<br>
@@ -38,6 +51,33 @@ const Fortnite_Stats = {
     Total_Wins: "${data.data.stats.all.overall.wins.toLocaleString()}",
 }; 
 \`\`\`
+<p align="center">
+\`\`\`js
+const Sudhan = {
+    FavouriteLanguage: "Javascript/Typescript",
+    OpenedIssues: {{ ISSUES }},
+    OpenedPullRequests: {{ PULL_REQUESTS }},
+    TotalCommits: {{ COMMITS }},
+    Stars: ${stars},
+    Repositories: {
+       Created: {{ REPOSITORIES }},
+       Contributed: {{ REPOSITORIES_CONTRIBUTED_TO }}
+    },
+}; //I'm a Epic Object, UwU
+\`\`\`
+</p>
+<p align="center">
+<img src="https://github-readme-streak-stats.herokuapp.com/?user=SudhanPlayz&theme=tokyonight">
+</p>
+<details>
+  <summary>
+      Even more stats
+  </summary>
+  <p align="center">
+    <img src="https://github-profile-trophy.vercel.app/?username=2M4U&theme=dracula">
+    <img src="https://github-readme-stats.vercel.app/api?username=2M4U&theme=tokyonight&count_private=true&show_icons=true&include_all_commits=true">
+  </p>
+</details>
 
 <!-- Last updated on ${date.toString()} ;-;-->
 <i>Last updated on ${date.getDate()}${
@@ -74,6 +114,6 @@ const Fortnite_Stats = {
 };
 
 (() => {
+  CountStars();
   WriteReadMe();
 })();
-
