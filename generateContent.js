3044ff03910f222dc5b978a1663e6b462e1c79fc
2M4U <cybercdn.host@gmail.com>
2023-06-21 23:54:34 +0100

    Update generateContent.js

--- generateContent.js
+++ generateContent.js
@@ -1,69 +1,68 @@
 class GenerateContent {
-    generateRepositorySection(repositories, owner, getStatisticsContent) {
-        let content = '';
+  generateRepositorySection(repositories, owner, getStatisticsContent) {
+    let content = '';
 
-        repositories.sort((repoA, repoB) => repoB.stargazers_count - repoA.stargazers_count);
+    repositories.sort((repoA, repoB) => repoB.stargazers_count - repoA.stargazers_count);
 
-        for (const repo of repositories) {
-            const repoName = repo.name;
-            const repoDescription = repo.description || '';
-            const repoTopics = repo.topics ? repo.topics.join(', ') : '';
+    content += '## Repositories\n\n';
+    content += '| Repository | Owner | Description | Topics | Stars | Forks | Open Issues | Watchers |\n';
+    content += '| --- | --- | --- | --- | --- | --- | --- | --- |\n';
 
-            content += `## ${repoName}\n\n`;
-            content += `**Owner**: ${owner}\n\n`;
-            content += `**Description**: ${repoDescription}\n\n`;
-            content += `**Topics**: ${repoTopics}\n\n`;
+    for (const repo of repositories) {
+      const repoName = repo.name;
+      const repoDescription = repo.description || '';
+      const repoTopics = repo.topics ? repo.topics.join(', ') : '';
+      const statistics = getStatisticsContent(owner, repoName);
 
-            content += '### Statistics\n\n';
-            content += `- **Stars**: ${repo.stargazers_count}\n`;
-            content += `- **Forks**: ${repo.forks_count}\n`;
-            content += `- **Open Issues**: ${repo.open_issues_count}\n`;
-            content += `- **Watchers**: ${repo.subscribers_count}\n\n`;
-
-            content += `![Stars](https://img.shields.io/github/stars/${owner}/${repoName}) `;
-            content += `![Forks](https://img.shields.io/github/forks/${owner}/${repoName}) `;
-            content += `![Open Issues](https://img.shields.io/github/issues/${owner}/${repoName})\n\n`;
-
-            content += '\n';
-        }
-
-        return content;
+      content += `| [${repoName}](https://github.com/${owner}/${repoName}) `;
+      content += `| ${owner} `;
+      content += `| ${repoDescription} `;
+      content += `| ${repoTopics} `;
+      content += `| ${statistics.Stars} `;
+      content += `| ${statistics.Forks} `;
+      content += `| ${statistics['Open Issues']} `;
+      content += `| ${statistics.Watchers} |\n`;
     }
 
-    generateLanguageSection(languageCount) {
-        let content = '';
+    return content;
+  }
 
-        if (Object.keys(languageCount).length > 0) {
-            content += '## Most Used Languages\n\n';
+  generateLanguageSection(languageCount) {
+    let content = '';
 
-            const sortedLanguages = Object.keys(languageCount).sort(
-                (a, b) => languageCount[b] - languageCount[a]
-            );
+    if (Object.keys(languageCount).length > 0) {
+      content += '## Most Used Languages\n\n';
 
-            for (const language of sortedLanguages) {
-                content += `- ${language}: ${languageCount[language]}\n`;
-            }
+      const sortedLanguages = Object.keys(languageCount).sort(
+        (a, b) => languageCount[b] - languageCount[a]
+      );
 
-            content += '\n';
-        }
+      for (const language of sortedLanguages) {
+        content += `- ${language}: ${languageCount[language]}\n`;
+      }
 
-        return content;
+      content += '---\n\n';
     }
 
-    generateReadmeContent(repositories, owner, getStatisticsContent, languageCount) {
-        let content = '';
+    return content;
+  }
 
-        let tableOfContents = '';
-        for (const repo of repositories) {
-            tableOfContents += `- [${repo.name}](#${repo.name.toLowerCase()})\n`;
-        }
+  generateReadmeContent(repositories, owner, getStatisticsContent, languageCount) {
+    let content = '';
 
-        content += `# GitHub Repository Statistics\n\n${tableOfContents}\n---\n\n`;
-        content += this.generateLanguageSection(languageCount);
-        content += this.generateRepositorySection(repositories, owner, getStatisticsContent);
-
-        return content;
+    let tableOfContents = '';
+    for (const repo of repositories) {
+      tableOfContents += `- [${repo.name}](#${repo.name.toLowerCase().replace(/ /g, '-')})\n`;
     }
+
+    content += '# GitHub Repository Statistics\n\n';
+    content += tableOfContents + '\n';
+    content += '---\n\n';
+    content += this.generateLanguageSection(languageCount);
+    content += this.generateRepositorySection(repositories, owner, getStatisticsContent);
+
+    return content;
+  }
 }
 
 module.exports = GenerateContent;
